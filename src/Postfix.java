7a7d42cd7af96db63a68e9ab896a00a7f656ad5c
Simon Larsén <slarse@kth.se>
2018-12-15 13:51:55 +0100

    Replace tabs with spaces, fix #2

--- src/Postfix.java
+++ src/Postfix.java
@@ -12,56 +12,56 @@
  * @version 2017-12-12
  */
 public class Postfix {
-	public static class ExpressionException extends Exception {
-		public ExpressionException(String message) {
-			super(message);
-		}
-	}
+    public static class ExpressionException extends Exception {
+        public ExpressionException(String message) {
+            super(message);
+        }
+    }
 
-	/**
-	 * Evaluates the given postfix expression.
-	 * 
-	 * @param expr  Arithmetic expression in postfix notation
-	 * @return      The value of the evaluated expression
-	 * @throws      ExpressionException if the expression is wrong
-	 */
-	public static int evaluate(String expr) throws ExpressionException {
-		// TODO
-		return 0;
-	}
-	
-	/**
-	 * Returns true if s is an operator.
-	 *
-	 * A word of caution on using the String.matches method: it returns true
-	 * if and only if the whole given string matches the regex. Therefore
-	 * using the regex "[0-9]" is equivalent to "^[0-9]$".
-	 *
-	 * An operator is one of '+', '-', '*', '/'.
-	 */
-	private static boolean isOperator(String s) {
-		// TODO
-		return false;
-	}
-	
-	/**
-	 * Returns true if s is an integer.
-	 *
-	 * A word of caution on using the String.matches method: it returns true
-	 * if and only if the whole given string matches the regex. Therefore
-	 * using the regex "[0-9]" is equivalent to "^[0-9]$".
-	 *
-	 * We accept two types of integers:
-	 *
-	 * - the first type consists of an optional '-' 
-	 *   followed by a non-zero digit
-	 *   followed by zero or more digits,
-	 *
-	 * - the second type consists of an optional '-'
-	 *   followed by a single '0'.
-	 */
-	private static boolean isInteger(String s) {
-		// TODO
-		return false;
-	}
+    /**
+     * Evaluates the given postfix expression.
+     *
+     * @param expr  Arithmetic expression in postfix notation
+     * @return      The value of the evaluated expression
+     * @throws      ExpressionException if the expression is wrong
+     */
+    public static int evaluate(String expr) throws ExpressionException {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * Returns true if s is an operator.
+     *
+     * A word of caution on using the String.matches method: it returns true
+     * if and only if the whole given string matches the regex. Therefore
+     * using the regex "[0-9]" is equivalent to "^[0-9]$".
+     *
+     * An operator is one of '+', '-', '*', '/'.
+     */
+    private static boolean isOperator(String s) {
+        // TODO
+        return false;
+    }
+
+    /**
+     * Returns true if s is an integer.
+     *
+     * A word of caution on using the String.matches method: it returns true
+     * if and only if the whole given string matches the regex. Therefore
+     * using the regex "[0-9]" is equivalent to "^[0-9]$".
+     *
+     * We accept two types of integers:
+     *
+     * - the first type consists of an optional '-'
+     *   followed by a non-zero digit
+     *   followed by zero or more digits,
+     *
+     * - the second type consists of an optional '-'
+     *   followed by a single '0'.
+     */
+    private static boolean isInteger(String s) {
+        // TODO
+        return false;
+    }
 }
