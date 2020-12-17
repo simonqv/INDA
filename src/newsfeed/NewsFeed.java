a3e98d99d34f7f564cf31fedf8d05c2676054468
glassey <glassey@kth.se>
2020-12-17 12:02:49 +0100

    Add newsfeed source code

---
+++ src/newsfeed/NewsFeed.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+/**
+ * The NewsFeed class stores news posts for the news feed in a
+ * social network application (like FaceBook or Google+).
+ *
+ * Display of the posts is currently simulated by printing the
+ * details to the terminal. (Later, this should display in a browser.)
+ *
+ * This version does not save the data to disk, and it does not
+ * provide any search or ordering functions.
+ *
+ * @author Michael Kölling and David J. Barnes
+ * @version 0.2
+ */
+public class NewsFeed {
+    private ArrayList<Post> posts;
+
+    /**
+     * Construct an empty news feed.
+     */
+    public NewsFeed() {
+        posts = new ArrayList<Post>();
+    }
+
+    /**
+     * Add a post to the news feed.
+     *
+     * @param post  The post to be added.
+     */
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    /**
+     * Show the news feed. Currently: print the news feed details
+     * to the terminal. 
+     */
+    public void show() {
+        // display all posts
+        for(Post post : posts) {
+            post.display();
+            System.out.println();
+        }
+    }
+}
